b2011100a24e21edd5ca595151b314bf230cb9e2
Anselm Peter <anselm.peter@web.de>
2024-02-13 16:58:50 +0100

    fixed and added bindings

--- msfs-plane-list-2.py
+++ msfs-plane-list-2.py
@@ -1,13 +1,17 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer
+from textual import events
 
 class Scanner(App):
   """A textual app to search for MSFS airplanes and
   export a list to Excel and/or CSV"""
 
-  BINDINGS = [('d', 'toggle dark', 'Toggle dark mode')]
+  BINDINGS = [
+    ('d', 'toggle_dark()', 'Toggle dark mode'),
+    ('q', 'quit()', 'End the program')
+  ]
 
-  def compose(self):
+  def compose(self) -> ComposeResult:
     yield Header()
     yield Footer()
 
